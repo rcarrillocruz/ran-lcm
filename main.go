110aa514fffdd720225ca58e2a162bd906937807
Ricardo Carrillo Cruz <ricarril@redhat.com>
2021-05-26 15:19:09 +0200

    Add SiteBackup api

--- main.go
+++ main.go
@@ -110,6 +110,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "GroupPolicyBinding")
 		os.Exit(1)
 	}
+	if err = (&controllers.SiteBackupReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("SiteBackup"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "SiteBackup")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
